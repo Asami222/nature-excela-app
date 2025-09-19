9dd06ac1c5b65e712305ed2a1369ac9a1768379f
Asami222 <asa2_pokecho@icloud.com>
2025-09-19 21:13:34 +0900

    9.19.2

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,18 +2,13 @@ import Header from "@/components/Header/Header";
 import HomeHero from "@/components/HomeHero/Home.Hero";
 import Footer from "@/components/Footer/Footer";
 import styles from "./layout.module.css";
-import face from 'public/home/face.webp'
-import faceMobile from 'public/home/faceMobile.webp'
-import logo from 'public/home/topLogo.svg'
-import logoMobile from 'public/home/topLogoMobile.svg'
-
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
       <>
         <div className={styles.headContainer}>
           <Header isHome />
-          <HomeHero face={face} faceMobile={faceMobile} logo={logo} logoMobile={logoMobile}/>
+          <HomeHero/>
         </div>
         <main className={styles.mainContainer}>{children}</main>
         <Footer isHome />
